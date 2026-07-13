3e5da53f5914038c30e60a27699aa796ee0290bc
MhN_AR <mahan85a@gmail.com>
2026-07-13 16:09:38 +0330

    feat: add DatabaseConfig

---
+++ src/ir/maktabsharif/event/util/DatabaseConfig.java
@@ -0,0 +1,22 @@
+package ir.maktabsharif.event.util;
+
+import ir.maktabsharif.event.exception.DatabaseConnectionException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConfig {
+    private static final String URL = "jdbc:postgresql://localhost:5432/hw17";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "2117";
+
+    public static Connection getConnection() {
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+            return connection;
+        }
+        catch (SQLException e) {
+            throw new DatabaseConnectionException("Connection to Database Failed!" + e.getMessage());
+        }
+    }
+}
