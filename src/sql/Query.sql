2359ca8988a7dca5bc9e4a5c87d809ca7d6861c7
MhN_AR <mahan85a@gmail.com>
2026-07-13 16:00:31 +0330

    feat: create database and tables

---
+++ src/sql/Query.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE hw17;
+
+CREATE TABLE events (
+                        id BIGSERIAL PRIMARY KEY ,
+                        title VARCHAR(50),
+                        location VARCHAR(200),
+                        capacity INTEGER,
+                        reserved_count INTEGER,
+                        ticket_price DECIMAL(15,2),
+                        status VARCHAR
+);
+
+CREATE TABLE reservations (
+                              id BIGSERIAL PRIMARY KEY ,
+                              customer_name VARCHAR(50),
+                              customer_phone VARCHAR(15),
+                              event_id BIGINT,
+                              ticket_count INTEGER,
+                              reservation_date DATE,
+                              status VARCHAR
+);
