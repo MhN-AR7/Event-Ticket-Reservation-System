f3b50bb9e431b4e5deb57e786bbdb5dc76280451
MhN_AR <mahan85a@gmail.com>
2026-07-14 00:00:52 +0330

    feat: add DatabaseRepositoryException

---
+++ src/ir/maktabsharif/event/exception/DatabaseRepositoryException.java
@@ -0,0 +1,7 @@
+package ir.maktabsharif.event.exception;
+
+public class DatabaseRepositoryException extends RuntimeException {
+    public DatabaseRepositoryException(String message) {
+        super(message);
+    }
+}
