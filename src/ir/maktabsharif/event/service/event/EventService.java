13789662865b3247b40d83f7c9f429e41b675d54
MhN_AR <mahan85a@gmail.com>
2026-07-14 01:03:11 +0330

    feat: add EventService Interface

---
+++ src/ir/maktabsharif/event/service/event/EventService.java
@@ -0,0 +1,7 @@
+package ir.maktabsharif.event.service.event;
+
+import ir.maktabsharif.event.model.Event;
+import ir.maktabsharif.event.service.GenericService;
+
+public interface EventService extends GenericService<Event> {
+}
