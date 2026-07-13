58a650b058f20b2ebb2c7727e3ace3a8959be217
MhN_AR <mahan85a@gmail.com>
2026-07-14 02:21:52 +0330

    feat: add ReservationService Interface

---
+++ src/ir/maktabsharif/event/service/reservation/ReservationService.java
@@ -0,0 +1,7 @@
+package ir.maktabsharif.event.service.reservation;
+
+import ir.maktabsharif.event.model.Reservation;
+import ir.maktabsharif.event.service.GenericService;
+
+public interface ReservationService extends GenericService<Reservation> {
+}
