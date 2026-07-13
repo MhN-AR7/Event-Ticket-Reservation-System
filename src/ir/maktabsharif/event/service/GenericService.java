28ad44830528a0b42fa415d82eaf41f05cc8331a
MhN_AR <mahan85a@gmail.com>
2026-07-14 01:23:47 +0330

    refactor: edit return type of change method

--- src/ir/maktabsharif/event/service/GenericService.java
+++ src/ir/maktabsharif/event/service/GenericService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface GenericService<T> {
     Long register(T t);
-    boolean change(T t);
+    T change(T t);
     boolean remove(Long id);
     boolean cancel(Long id);
     T getById(Long id);
