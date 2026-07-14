ca73e52c1f194c21273c82dcb1b9bcc2168ca411
MhN_AR <mahan85a@gmail.com>
2026-07-14 14:13:52 +0330

    feat: add findAverageTicketPrice method to EventRepository

--- src/ir/maktabsharif/event/repository/event/EventRepository.java
+++ src/ir/maktabsharif/event/repository/event/EventRepository.java
@@ -3,9 +3,11 @@ package ir.maktabsharif.event.repository.event;
 import ir.maktabsharif.event.model.Event;
 import ir.maktabsharif.event.repository.GenericRepository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface EventRepository extends GenericRepository<Event> {
     Integer findActiveEventCount();
     List<Event> findMostExpensive();
+    BigDecimal findAverageTicketPrice();
 }
