d492e957cb0d159bb04f82cb8e35f4fbc84d6c2f
MhN_AR <mahan85a@gmail.com>
2026-07-14 14:14:42 +0330

    feat: add getAverageTicketPrice method to EventService

--- src/ir/maktabsharif/event/service/event/EventService.java
+++ src/ir/maktabsharif/event/service/event/EventService.java
@@ -3,9 +3,11 @@ package ir.maktabsharif.event.service.event;
 import ir.maktabsharif.event.model.Event;
 import ir.maktabsharif.event.service.GenericService;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface EventService extends GenericService<Event> {
     Integer getActiveEventCount();
     List<Event> getMostExpensive();
+    BigDecimal getAverageTicketPrice();
 }
