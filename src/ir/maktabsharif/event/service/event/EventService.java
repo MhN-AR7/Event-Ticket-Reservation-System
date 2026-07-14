bb481fb7abfc67969d629ef78711be5f662d7ea2
MhN_AR <mahan85a@gmail.com>
2026-07-14 14:07:48 +0330

    feat: add getMostExpensive method to EventService

--- src/ir/maktabsharif/event/service/event/EventService.java
+++ src/ir/maktabsharif/event/service/event/EventService.java
@@ -3,6 +3,9 @@ package ir.maktabsharif.event.service.event;
 import ir.maktabsharif.event.model.Event;
 import ir.maktabsharif.event.service.GenericService;
 
+import java.util.List;
+
 public interface EventService extends GenericService<Event> {
     Integer getActiveEventCount();
+    List<Event> getMostExpensive();
 }
