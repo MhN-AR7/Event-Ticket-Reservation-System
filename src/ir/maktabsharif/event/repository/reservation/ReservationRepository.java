315429c90999b5871ec3c62caee4c2c0caf048f0
MhN_AR <mahan85a@gmail.com>
2026-07-14 14:22:56 +0330

    feat: add findActiveReservation method to ReservationRepository

--- src/ir/maktabsharif/event/repository/reservation/ReservationRepository.java
+++ src/ir/maktabsharif/event/repository/reservation/ReservationRepository.java
@@ -3,6 +3,9 @@ package ir.maktabsharif.event.repository.reservation;
 import ir.maktabsharif.event.model.Reservation;
 import ir.maktabsharif.event.repository.GenericRepository;
 
+import java.util.List;
+
 public interface ReservationRepository extends GenericRepository<Reservation> {
     boolean existByPhoneNumber(String phoneNumber);
+    List<Reservation> findActiveReservation();
 }
